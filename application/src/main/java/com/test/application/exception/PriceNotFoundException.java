9c5ed05e022763e333aed550b9ca78a60f18c3be
iv4n89 <ivan.betanzos89@gmail.com>
2025-08-07 23:35:50 +0200

    feat: application exception

---
+++ application/src/main/java/com/test/application/exception/PriceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.test.application.exception;
+
+public final class PriceNotFoundException extends RuntimeException {
+    public PriceNotFoundException(String message) {
+        super(message);
+    }
+
+    public PriceNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
