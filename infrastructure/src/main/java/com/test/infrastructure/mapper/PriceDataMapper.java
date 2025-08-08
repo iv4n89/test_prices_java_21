988092f7b29967ac245e6856ead141cfc813f025
iv4n89 <ivan.betanzos89@gmail.com>
2025-08-08 02:08:19 +0200

    feat: mapper

---
+++ infrastructure/src/main/java/com/test/infrastructure/mapper/PriceDataMapper.java
@@ -0,0 +1,31 @@
+package com.test.infrastructure.mapper;
+
+import com.test.domain.model.PriceModel;
+import com.test.domain.valueObject.*;
+import com.test.infrastructure.entity.PriceEntity;
+
+public final class PriceDataMapper {
+    public static PriceModel toDomainModel(PriceEntity priceEntity) {
+        return PriceModel.builder()
+                .brandId(new BrandId(priceEntity.getBrandId()))
+                .productId(new ProductId(priceEntity.getProductId()))
+                .startDate(priceEntity.getStartDate())
+                .endDate(priceEntity.getEndDate())
+                .price(new Money(priceEntity.getPrice()))
+                .currency(new Currency(priceEntity.getCurr()))
+                .priority(new Priority(priceEntity.getPriority()))
+                .build();
+    }
+
+    public static PriceEntity toEntity(PriceModel priceModel) {
+        return PriceEntity.builder()
+                .brandId(priceModel.getBrandId().getValue())
+                .productId(priceModel.getProductId().getValue())
+                .startDate(priceModel.getStartDate())
+                .endDate(priceModel.getEndDate())
+                .price(priceModel.getPrice().getValue())
+                .curr(priceModel.getCurrency().getValue())
+                .priority(priceModel.getPriority().getValue())
+                .build();
+    }
+}
